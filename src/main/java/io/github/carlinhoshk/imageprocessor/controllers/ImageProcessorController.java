45fa926fd2f0bebd220cbfdb277ca6c3e4c2201a
carlinhoshk <carlosmdohk@gmail.com>
2023-09-25 18:58:05 -0300

    Feat: aplicando controller e service para subir foto e salvar

--- src/main/java/io/github/carlinhoshk/imageprocessor/controllers/ImageProcessorController.java
+++ src/main/java/io/github/carlinhoshk/imageprocessor/controllers/ImageProcessorController.java
@@ -1,4 +1,33 @@
 package io.github.carlinhoshk.imageprocessor.controllers;
 
+import io.github.carlinhoshk.imageprocessor.services.ImageProcessorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("image")
 public class ImageProcessorController {
+    static final String targetPath = "./src/main/resources/data/output/output.png";
+
+    @Autowired
+    private ImageProcessorService imageService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) {
+        try {
+
+            imageService.processImageAndSave(file, targetPath);
+            return ResponseEntity.ok("Imagem processada e salva com sucesso.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Erro ao processar e salvar a imagem: " + e.getMessage());
+        }
+    }
 }
+
--- src/main/java/io/github/carlinhoshk/imageprocessor/services/ImageProcessorService.java
+++ src/main/java/io/github/carlinhoshk/imageprocessor/services/ImageProcessorService.java
@@ -1,20 +1,65 @@
 package io.github.carlinhoshk.imageprocessor.services;
 
-import org.opencv.core.Mat;
+import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.objdetect.Objdetect;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Service
 public class ImageProcessorService {
 
-
-    public static Mat loadImage(String imagePath) {
-        Imgcodecs imageCodecs = new Imgcodecs();
-        return imageCodecs.imread(imagePath);
+    public Mat loadImage(MultipartFile file) {
+        try {
+            byte[] bytes = file.getBytes();
+            return Imgcodecs.imdecode(new MatOfByte(bytes), Imgcodecs.IMREAD_COLOR);
+        } catch (IOException e) {
+            throw new RuntimeException("Erro ao carregar a imagem.", e);
+        }
     }
 
-    public static void saveImage(Mat imageMatrix, String targetPath) {
+    public void processImageAndSave(MultipartFile file, String targetDirectory) {
+        Mat loadedImage = loadImage(file);
+        MatOfRect facesDetected = new MatOfRect();
+
+        CascadeClassifier cascadeClassifier = new CascadeClassifier();
+        int minFaceSize = Math.round(loadedImage.rows() * 0.1f);
+        cascadeClassifier.load("src/main/resources/haarcascades/haarcascade_frontalface_alt.xml");
+        cascadeClassifier.detectMultiScale(loadedImage,
+                facesDetected,
+                1.1,
+                3,
+                Objdetect.CASCADE_SCALE_IMAGE,
+                new Size(minFaceSize, minFaceSize),
+                new Size()
+        );
+
+        Rect[] facesArray = facesDetected.toArray();
+        for (Rect face : facesArray) {
+            Imgproc.rectangle(loadedImage, face.tl(), face.br(), new Scalar(0, 0, 255), 3);
+        }
+
+        // Obtém a data e hora atuais
+        LocalDateTime currentDateTime = LocalDateTime.now();
+
+        // Formata a data e hora atual como parte do nome do arquivo
+        String fileName = currentDateTime.format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss")) + ".jpg";
+
+        // Caminho completo para o arquivo de destino
+        String targetPath = Paths.get(targetDirectory, fileName).toString();
+
+        saveImage(loadedImage, targetPath);
+    }
+
+    private void saveImage(Mat imageMatrix, String targetPath) {
         Imgcodecs imgcodecs = new Imgcodecs();
         imgcodecs.imwrite(targetPath, imageMatrix);
     }
-
-
 }
